e1384f542ef7c308b529a70dfba95deec16705be
MJAsprec <michael.asprec@whitecloak.com>
2024-02-21 23:16:05 +0800

    update: user/nft entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { NftsModule } from './nfts/nfts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, NftsModule],
+  imports: [
+    UsersModule,
+    NftsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nft-marketplace',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
