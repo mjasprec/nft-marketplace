33bed118c55146f2fa411a86d9a70d9a7d5a645d
MJAsprec <michael.asprec@whitecloak.com>
2024-02-23 12:34:49 +0800

    update: get all nfts and get nft by id

--- src/nfts/nfts.controller.ts
+++ src/nfts/nfts.controller.ts
@@ -1,10 +1,33 @@
 import {
   Controller,
-  // Body, Delete, Get, Param, Post
+  Post,
+  Body,
+  Get,
+  Param,
+  // Delete,
 } from '@nestjs/common';
-// import { NftsService } from './nfts.service';
-// import { CreateNftDTO } from './dto/create-nft.dto';
+import { NftsService } from './nfts.service';
+import { CreateNftDTO } from './dto/create-nft.dto';
+import { NftEntity } from './nft.entity';
+
 // import { GetNftFilterDTO } from './dto/get-nft-filter.dto';
 
 @Controller('nfts')
-export class NftsController {}
+export class NftsController {
+  constructor(private nftsServices: NftsService) {}
+
+  @Get('/')
+  getNfts(): Promise<NftEntity[]> {
+    return this.nftsServices.getNfts();
+  }
+
+  @Get('/:id')
+  getNftById(@Param('id') id: string): Promise<NftEntity> {
+    return this.nftsServices.getNftByID(id);
+  }
+
+  @Post('/')
+  createNft(@Body() createNftDto: CreateNftDTO): Promise<NftEntity> {
+    return this.nftsServices.createNft(createNftDto);
+  }
+}
--- src/nfts/nfts.service.ts
+++ src/nfts/nfts.service.ts
@@ -1,11 +1,62 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { NftEntity } from './nft.entity';
+import { CreateNftDTO } from './dto/create-nft.dto';
 import {
-  Injectable,
-  // NotFoundException
-} from '@nestjs/common';
-// import { NftCategory, NftStatus } from './nfts.model';
+  // NftCategory,
+  NftStatus,
+} from './nfts.model';
 // import { v4 as uuid } from 'uuid';
 // import { CreateNftDTO } from './dto/create-nft.dto';
 // import { GetNftFilterDTO } from './dto/get-nft-filter.dto';
 
 @Injectable()
-export class NftsService {}
+export class NftsService extends Repository<NftEntity> {
+  constructor(private dataSource: DataSource) {
+    super(NftEntity, dataSource.createEntityManager());
+  }
+
+  async getNfts(): Promise<NftEntity[]> {
+    return await this.find({
+      where: {
+        status: NftStatus.ENABLED,
+        deletedAt: null,
+      },
+    });
+  }
+
+  async getNftByID(id: string): Promise<NftEntity> {
+    const matchedNft = await this.findOne({ where: { id } });
+
+    if (!matchedNft) {
+      throw new NotFoundException('No matching NFT found');
+    }
+
+    return matchedNft;
+  }
+
+  async createNft(createNftDto: CreateNftDTO): Promise<NftEntity> {
+    const { image, title, description, category, owner, creator } =
+      createNftDto;
+
+    const price = Number(createNftDto.price);
+
+    const newNft = this.create({
+      image,
+      title,
+      description,
+      category,
+      price,
+      owner,
+      creator,
+      comments: [],
+      status: NftStatus.ENABLED,
+    });
+
+    await this.save(newNft);
+
+    console.log('newNft', newNft);
+
+    return newNft;
+  }
+}
