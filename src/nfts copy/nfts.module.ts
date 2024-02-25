6b71d7ee136680e12b95ca1580a5572821be6160
MJAsprec <michael.asprec@whitecloak.com>
2024-02-25 22:33:44 +0800

    remove copies

--- src/nfts copy/nfts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { NftsService } from './nfts.service';
-import { NftsController } from './nfts.controller';
-
-@Module({
-  providers: [NftsService],
-  controllers: [NftsController]
-})
-export class NftsModule {}
