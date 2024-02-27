90edd584d285741cba0b9d941bcab78d15795fb9
MJAsprec <michael.asprec@whitecloak.com>
2024-02-27 17:06:24 +0800

    nestjs config cross env and env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { UsersModule } from './users/users.module';
 import { NftsModule } from './nfts/nfts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+
+console.log('PROCESS ENV', process.env.STAGE);
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`env.${process.env.STAGE}`],
+    }),
     UsersModule,
     NftsModule,
     TypeOrmModule.forRoot({
--- src/nfts/nfts.module.ts
+++ src/nfts/nfts.module.ts
@@ -4,9 +4,10 @@ import { NftsController } from './nfts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NftEntity } from './nft.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([NftEntity]), AuthModule],
+  imports: [ConfigModule, TypeOrmModule.forFeature([NftEntity]), AuthModule],
   providers: [NftsService],
   controllers: [NftsController],
 })
